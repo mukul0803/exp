35a72825d11598427b2bfebab25b4aae516cc6d0
Mukul Saini <51281638+mukul0803@users.noreply.github.com>
2023-09-15 18:55:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=> {
+
+    // res.send("Hello");
+    res.render('index');
+    
+});
+
+
+
+app.listen(4000);
